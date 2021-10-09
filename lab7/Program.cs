7b2707633c5706c5accd4d392a8bb7ebe5c9598e
user <user@WIN-2QU304JCFKP>
2021-10-09 19:22:43 +0200

    Добавьте файлы проекта.

---
+++ lab7/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double s = CalcS(a, b, c);
+
+            double s1 = s;
+            Console.WriteLine("Площадь первого  треугольника: {0}",s1);
+
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            s = CalcS(a, b, c);
+
+            double s2 = s;
+            Console.WriteLine("Площадь второго треугольника: {0}", s2);
+
+
+            //сравниваем площади
+            if (s1>s2)
+            {
+                Console.WriteLine("Площадь первого треугольника больше");
+            }
+            else
+            {
+                if (s1 == s2)
+                {
+                    Console.WriteLine("Площади треугольников равны");
+                }
+                else
+                {
+                    if (s1 < s2)
+                    {
+                        Console.WriteLine("Площадь второго треугольника больше");
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        //метод расчета площади
+        static double CalcS(int a, int b, int c)
+        {
+            double p = (a+b+c)/2;
+            double s = Math.Sqrt(p * (p - a)*(p-b)*(p-c));
+            return s;
+            
+        }
+    }
+}
---
+++ zadanie2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rebro = Convert.ToInt32(Console.ReadLine());
+            double v = Calc_v(rebro);
+            double s = Calc_s(rebro);
+
+            Console.WriteLine("Объем равен {0}", v);
+            Console.WriteLine("Площадь равна {0}", s);
+            Console.ReadKey();
+        }
+
+        static double Calc_v(int rebro)
+        {
+            double v = Math.Pow(rebro, 3);
+            return v;
+        }
+
+        static double Calc_s(int rebro)
+        {
+            double s = (Math.Pow(rebro,2))*6;
+            return s;
+        }
+    }
+}
